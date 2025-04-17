6b52708e2a7f6b4796ae7566c1178061422571b6
robxxt <personal.robert@protonmail.com>
2025-04-17 13:59:39 +0200

    make more secure request

--- pydantic/practice/.task1.py
+++ pydantic/practice/.task1.py
@@ -1,13 +1,23 @@
 from pydantic import BaseModel
 import requests
+import sys
 
 class User(BaseModel):
     username: str
     email: str
     website: str
 
+"""
+Please note that the BASE_URL shouldn't be visible like in this case.
+Normally you would take it from the environment variables or a .env file
+"""
 BASE_URL = "https://jsonplaceholder.typicode.com/users"
-unprocessed_users_list = requests.get(BASE_URL).json()
-processed_users_list = [User(**user_info) for user_info in unprocessed_users_list]
-
-print(processed_users_list[:3])
+try:
+    response = requests.get(BASE_URL)
+    if not response.ok:
+        raise ValueError(f"status_code: {response.status_code}")
+    unprocessed_users_list = response.json()
+    processed_users_list = [User(**user_info) for user_info in unprocessed_users_list]
+    print(processed_users_list[:3])
+except Exception as e:
+    sys.exit(f"Failed to establish connection with {BASE_URL}\n{e}")
